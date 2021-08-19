79612145207c16e545bd69f2c24a348349de5e12
Bluenix <bluenixdev@gmail.com>
2021-08-19 15:41:05 +0200

    Add ApplicationState to RESTClient
    add imports

--- wumpy/state/rest.py
+++ wumpy/state/rest.py
@@ -1,4 +1,10 @@
+from typing import TYPE_CHECKING
+
 from ..rest.requester import Requester, Route
+
+if TYPE_CHECKING:
+    from .state import ApplicationState
+
 
 __all__ = ('RESTClient',)
 
@@ -6,9 +12,14 @@ __all__ = ('RESTClient',)
 class RESTClient(Requester):
     """Requester subclass wrapping endpoints used for Discord applications."""
 
-    def __init__(self, token: str, *args, **kwargs):
+    _state: 'ApplicationState'
+
+    __slots__ = ('_state',)
+
+    def __init__(self, state: 'ApplicationState', token: str, *args, **kwargs):
         super().__init__(*args, **kwargs, headers={"Authorization": f"Bot {token}"})
 
+        self._state = state
     # Asset endpoint
 
     async def read_asset(self, url: str, *, size: int) -> bytes:
