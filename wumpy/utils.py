b763e38bd59f4390bbc5aeb510ba3dafed21fd87
Bluenix <bluenixdev@gmail.com>
2021-10-13 21:58:14 +0200

    Start working on an EventDispatcher

--- wumpy/utils.py
+++ wumpy/utils.py
@@ -22,11 +22,16 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import Any, BinaryIO
+import inspect
+from typing import (
+    Any, BinaryIO, Callable, ClassVar, Coroutine, Dict, List, Optional,
+    Tuple, Type, TypeVar, Union, overload
+)
 
+import anyio.abc
 from typing_extensions import Final, final
 
-__all__ = ('MISSING', 'File')
+__all__ = ('MISSING', 'File', 'Event', 'EventDispatcher')
 
 
 @final
@@ -71,3 +76,76 @@ class File:
 
     def close(self) -> None:
         return self.source.close()
+
+
+class Event:
+    """Subclass for events, meant to be read from annotations."""
+
+    _name: ClassVar[str]
+
+    def __init__(self, data: Any) -> None:
+        raise NotImplementedError()
+
+
+C = TypeVar('C', bound='Callable[[Event], Coroutine]')
+
+
+class EventDispatcher:
+    """Mixin to be able to dispatch events."""
+
+    listeners: Dict[str, List[Tuple[Type[Event], Callable[[Event], Coroutine]]]]
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.listeners = {}
+
+    def dispatch(self, event: str, *args: Any, tg: anyio.abc.TaskGroup) -> None:
+        """Dispatch appropriate listeners.
+
+        The callbacks will be started with the task group and passed the
+        arguments given.
+
+        Parameters:
+            event: The event to dispatch
+            *args: Arguments to pass onto the Event initializer
+            tg: Task group to start the callbacks with
+        """
+        for initializer, callback in self.listeners.get(event, []):
+            tg.start_soon(callback, initializer(*args))
+
+    def add_listener(self, callback: Callable[[Event], Coroutine]) -> None:
+        signature = inspect.signature(callback)
+
+        if len(signature.parameters) != 1:
+            raise TypeError(f'Listener callback should have one argument, not {signature}')
+
+        # There should be one argument in the parameters, which means it is
+        # safe for us to convert it to a list and grab the first item.
+        annotation = list(signature.parameters.values())[0].annotation
+
+        if not issubclass(annotation, Event):
+            raise TypeError('Listener argument annotation should be a subclass of Event')
+
+        if annotation._name in self.listeners:
+            self.listeners[annotation._name].append((annotation, callback))
+        else:
+            self.listeners[annotation._name] = [(annotation, callback)]
+
+    @overload
+    def listener(self, func: C, /) -> C:
+        ...
+
+    @overload
+    def listener(self) -> Callable[[C], C]:
+        ...
+
+    def listener(self, callback: Optional[C] = None, /) -> Union[Callable[[C], C], C]:
+        def decorator(func: C) -> C:
+            self.add_listener(func)
+            return func
+
+        if callback is not None:
+            return decorator(callback)
+
+        return decorator
