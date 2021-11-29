6a27d498f1bc999906cfcba4e08bc4aece08feb5
Bluenix <bluenixdev@gmail.com>
2021-11-30 00:00:07 +0100

    Add wumpy-gateway module docstring

--- library/wumpy-gateway/wumpy/gateway/__init__.py
+++ library/wumpy-gateway/wumpy/gateway/__init__.py
@@ -1,2 +1,24 @@
+"""Gateway implementation for the Wumpy project.
+
+This module contains a main `Shard` class, to get started import it and use the
+`connect()` classmethod as an asynchronous context manager:
+
+```python
+from wumpy.gateway import Shard
+
+
+TOKEN = 'ABC123.XYZ789'
+INTENTS = 65535
+
+
+async def main():
+    # Connect to the URI wss://gateway.discord.gg/ with the token
+    # ABC123.XYZ8789 and all intents.
+    async with Shard.connect('wss://gateway.discord.gg/', TOKEN, INTENTS) as ws:
+        async for event in ws:
+            print(event)  # The deserialized JSON event payload
+```
+"""
+
 from .errors import *
 from .shard import *
