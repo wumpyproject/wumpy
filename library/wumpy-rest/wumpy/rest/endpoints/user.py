0ba3252eb4fc8c30dfd0c58cbe12893b1920ab9b
Bluenix <bluenixdev@gmail.com>
2021-12-06 18:03:35 +0100

    Fill out user endpoints' docstrings and return types

--- library/wumpy-rest/wumpy/rest/endpoints/user.py
+++ library/wumpy-rest/wumpy/rest/endpoints/user.py
@@ -1,24 +1,41 @@
-from typing import Any, Dict, List, SupportsInt
+from typing import Any, Dict, List, Optional, SupportsInt
+
+from discord_typings import DMChannelData, GuildData, UserData
 
 from ..ratelimiter import Route
 from ..utils import MISSING
 from .base import Requester
 
+__all__ = ('UserRequester',)
+
 
 class UserRequester(Requester):
+    """Requester for Discord user endpoints."""
+
+    __slots__ = ()
+
     async def fetch_my_user(self) -> Dict[str, Any]:
         """Fetch the bot user account.
 
         This is not a shortcut to `fetch_user()`, it has a different ratelimit
         and returns a bit more information.
+
+        Returns:
+            The user object received from Discord.
         """
         return await self.request(Route('GET', '/users/@me'))
 
-    async def fetch_user(self, user: SupportsInt) -> Dict[str, Any]:
-        """Fetch a user by its ID.
+    async def fetch_user(self, user: SupportsInt) -> UserData:
+        """Fetch a data about a user by its ID.
 
-        You do not need to share a guild with the user to fetch their
+        You also do not need to share a guild with the user to fetch their
         (limited) information.
+
+        Parameters:
+            user: The ID of the user to fetch.
+
+        Returns:
+            The user object received from Discord.
         """
         return await self.request(Route('GET', '/users/{user_id}', user_id=int(user)))
 
@@ -26,37 +43,79 @@ class UserRequester(Requester):
         self,
         *,
         username: str = MISSING,
-        avatar: str = MISSING,
-    ) -> Dict[str, Any]:
+        avatar: Optional[str] = MISSING,
+    ) -> UserData:
         """Edit the bot user account.
 
-        `avatar` is an optional string, passing None will set the user's avatar
-        to its default avatar.
+        Parameters:
+            username:
+                The new bot user's name (can cause the discriminator to
+                be randomized if there are already a user with that name).
+            avatar:
+                Image data in the Data URI scheme, or None to reset the avatar
+                to the default Discord version according to the discriminator.
+
+        Returns:
+            The updated and new user object from Discord.
         """
         if username is MISSING and avatar is MISSING:
             raise TypeError("at least one of 'username' or 'avatar' is required")
 
-        payload: Dict[str, Any] = {
+        payload = {
             'username': username,
             'avatar': avatar,
         }
 
         return await self.request(Route('PATCH', '/users/@me'), json=payload)
 
-    async def fetch_my_guilds(self) -> List[Dict[str, Any]]:
-        """Fetch all guilds that the bot user is in."""
+    async def fetch_my_guilds(
+        self,
+        *,
+        before: SupportsInt = MISSING,
+        after: SupportsInt = MISSING,
+        limit: int = 200
+    ) -> List[GuildData]:
+        """Fetch all guilds that the bot user is in.
+
+        This endpoint allows pagination for bots who are a member of more than
+        200 guilds using the `before` and `after` parameters.
+
+        Parameters:
+            before: Snowflake to fetch guilds before.
+            after: Snowflake to fetch guilds after.
+            limit: How many guilds to maximally return.
+
+        Returns:
+            A list of partial guilds the bot user is a part of.
+        """
+        params = {'limit': limit}
+        if before is not MISSING:
+            params['before'] = int(before)
+        elif after is not MISSING:
+            params['after'] = int(after)
+
         return await self.request(Route('GET', '/users/@me/guilds'))
 
     async def leave_guild(self, guild: SupportsInt) -> None:
-        """Make the bot user leave the specified guild."""
+        """Make the bot user leave the specified guild.
+
+        Parameters:
+            guild: The ID of the guild to leave.
+        """
         await self.request(Route('DELETE', '/users/@me/guilds/{guild_id}', guild_id=int(guild)))
 
-    async def create_dm(self, recipient: SupportsInt) -> Dict[str, Any]:
+    async def create_dm(self, recipient: SupportsInt) -> DMChannelData:
         """Create a DM with the recipient.
 
         This method is safe to call several times to get the DM channel when
         needed. In fact, in other wrappers this is called everytime you send
         a message to a user.
+
+        Parameters:
+            recipient: The user to open a DM with.
+
+        Returns:
+            The DM channel object returned by Discord.
         """
         return await self.request(Route(
             'POST', '/users/@me/channels'), json={'recipient_id': int(recipient)}
