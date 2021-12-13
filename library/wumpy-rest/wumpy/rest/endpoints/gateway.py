110c3d244e282d620131fa000e76328e020d78c3
Bluenix <bluenixdev@gmail.com>
2021-12-13 19:12:32 +0100

    Fill out GatewayRequester docstrings

--- library/wumpy-rest/wumpy/rest/endpoints/gateway.py
+++ library/wumpy-rest/wumpy/rest/endpoints/gateway.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from discord_typings import GetGatewayBotData
 
 from ..route import Route
 from .base import Requester
@@ -9,13 +9,20 @@ class GatewayRequester(Requester):
         """Fetch a single valid WSS URL.
 
         The data this method returns should be cached and will not change.
+
+        Returns:
+            The URL to connect with a WebSocket to.
         """
         d = await self.request(Route('GET', '/gateway'))
         return d['url']
 
-    async def fetch_gateway_bot(self) -> Dict[str, Any]:
+    async def fetch_gateway_bot(self) -> GetGatewayBotData:
         """Fetch a valid WSS URL along with helpful metadata.
 
-        Especially useful when running big bots with shards.
+        Especially useful when running big bots with shards, the data returned
+        by this call can change per-call as the bot joins/leaves guilds.
+
+        Returns:
+            Useful metadata about the gateway when connecting.
         """
         return await self.request(Route('GET', '/gateway/bot'))
