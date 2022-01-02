7384b9409e1ec5681ab66c816ae18864ac0b09fc
Bluenix <bluenixdev@gmail.com>
2022-01-02 23:23:00 +0100

    Add GuildTemplateRequester to APIClient

--- library/wumpy-rest/wumpy/rest/endpoints/__init__.py
+++ library/wumpy-rest/wumpy/rest/endpoints/__init__.py
@@ -1,13 +1,14 @@
 from .base import *
 from .channel import *
 from .gateway import *
+from .guild_template import *
 from .sticker import *
 from .user import *
 from .webhook import *
 
 
-class APIClient(ChannelRequester, GatewayRequester, StickerRequester, UserRequester,
-                WebhookRequester):
+class APIClient(ChannelRequester, GatewayRequester, GuildTemplateRequester, StickerRequester,
+                UserRequester, WebhookRequester):
     """Requester class with all endpoints inherited."""
 
     __slots__ = ()
