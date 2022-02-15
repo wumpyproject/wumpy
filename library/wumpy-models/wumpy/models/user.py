e1e6ecfd6232f349f664a8cefa10892cf14a271b
Bluenix <bluenixdev@gmail.com>
2022-02-15 21:40:00 +0100

    Switch user classes to use dataclasses

--- library/wumpy-models/wumpy/models/user.py
+++ library/wumpy-models/wumpy/models/user.py
@@ -1,100 +1,66 @@
-from typing import Any, Dict, Optional
+import dataclasses
 
-from .base import Object
+from discord_typings import UserData
+from typing_extensions import Self
+
+from .base import Model
 from .flags import UserFlags
 
-__all__ = ('InteractionUser', 'BotUser', 'User')
+__all__ = ('BotUser', 'User')
 
 
-class _BaseUser(Object):
-    """The base for all user objects.
-
-    The reason that this is seperated from User is because we don't want
-    BotUser to inherit certain methods, you cannot DM yourself for example.
-    """
-
+@dataclasses.dataclass(frozen=True, eq=False)
+class User(Model):
     name: str
     discriminator: int
-    avatar: Optional[str]
-
     public_flags: UserFlags
 
-    bot: Optional[bool]
-    system: Optional[bool]
+    bot: bool
+    system: bool
 
-    __slots__ = (
-        'name', 'discriminator', 'avatar', 'public_flags', 'bot', 'system'
-    )
-
-    def __init__(self, data: Dict[str, Any]) -> None:
-        super().__init__(int(data['id']))
-
-        self.bot = None
-        self.system = None
-
-        # We may need to update this object again, so if we seperate it
-        # into another method we can call again
-        self._update(data)
-
-    def _update(self, data: Dict) -> None:
-        self.name = data['username']
-        self.discriminator = int(data['discriminator'])
-
-        self.avatar = data['avatar']
-
-        self.public_flags = UserFlags(data['public_flags'])
-
-        self.bot = data.get('bot', self.bot)
-        self.system = data.get('system', self.system)
+    def __str__(self) -> str:
+        return f'{self.name}#{self.discriminator}'
 
     @property
     def mention(self) -> str:
         return f'<@{self.id}>'
 
-    @property
-    def default_avatar(self) -> int:
-        # It is unneccessary to keep the default asset for each user in
-        # memory, we create them when asked for.
-        return self.discriminator % 5
+    @classmethod
+    def from_data(cls, data: UserData) -> Self:
+        return cls(
+            id=int(data['id']),
+            name=data['username'],
+            discriminator=int(data['discriminator']),
+            bot=data.get('bot', False),
+            system=data.get('system', False),
+            public_flags=UserFlags(data.get('public_flags', 0))
+        )
 
 
-class InteractionUser(_BaseUser):
-    """User object from an interaction, this wraps no user endpoints."""
-    pass
-
-
-class BotUser(_BaseUser):
-    """User object attached to an application; the bot application's user account."""
-
-    bio: str
+@dataclasses.dataclass(frozen=True, eq=False)
+class BotUser(User):
     locale: str
     mfa_enabled: bool
     verified: bool
 
-    __slots__ = ('bio', 'locale', 'mfa_enabled', 'verified')
+    @classmethod
+    def from_data(cls, data: UserData) -> Self:
+        if (
+            'locale' not in data
+            or 'mfa_enabled' not in data
+            or 'verified' not in data
+        ):
+            raise ValueError('Cannot create a BotUser from data without extra user fields')
 
-    def __init__(self, data: Dict) -> None:
-        super().__init__(data)
+        return cls(
+            id=int(data['id']),
+            name=data['username'],
+            discriminator=int(data['discriminator']),
+            public_flags=UserFlags(data.get('public_flags', 0)),
+            bot=data.get('bot', False),
+            system=data.get('system', False),
 
-        # We may need to update this object again, so if we seperate it
-        # into another method we can call again
-        self._update(data)
-
-    def _update(self, data: Dict) -> None:
-        super()._update(data)
-
-        self.bio = data['bio']
-        self.locale = data['locale']
-
-        self.mfa_enabled = data['mfa_enabled']
-        self.verified = data['verified']
-
-    @property
-    def flags(self):
-        return self.public_flags
-
-
-class User(_BaseUser):
-    """Discord User object."""
-
-    __slots__ = ()
+            locale=data['locale'],
+            mfa_enabled=data['mfa_enabled'],
+            verified=data['verified'],
+        )
