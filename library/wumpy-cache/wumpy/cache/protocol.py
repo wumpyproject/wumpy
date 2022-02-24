feadd4b3f67c84b10d11e4d94b2814c9e6620bd3
Bluenix <bluenixdev@gmail.com>
2022-02-24 14:20:30 +0100

    Add cache methods

--- library/wumpy-cache/wumpy/cache/protocol.py
+++ library/wumpy-cache/wumpy/cache/protocol.py
@@ -1,6 +1,10 @@
-from typing import Any, Optional, Protocol, SupportsInt
+from typing import (
+    Any, AsyncContextManager, Dict, Optional, Protocol, SupportsInt, Tuple
+)
 
-import anyio.lowlevel
+from wumpy.models import (
+    Channel, Emoji, Guild, Invite, Member, Message, Role, Sticker, User
+)
 
 __all__ = ('Cache',)
 
@@ -8,31 +12,37 @@ __all__ = ('Cache',)
 class Cache(Protocol):
     """Protocol for a cache implementation for Wumpy.
 
-    Another benefit this class gives is that it can be overriden to get
-    all necessary methods defined with no implementation provided. By default
-    all methods will simply yield to the event loop and return None.
+    This is not the default implementation for the cache, rather it is the
+    typehint that should be used for the cache. All methods return None, so
+    this can be used if you do not wish to implement a cache.
     """
 
-    async def update(self, payload: dict) -> Optional[Any]:
+    async def update(self, payload: Dict[str, Any]) -> Tuple[Optional[Any], Optional[Any]]:
         """Update the cache with new information from an event.
 
-        This method should return the older data that was in the cache, so that
-        it can be dispatched to the user's event listeners. Because of the huge
-        amounts of types of events that Discord sends it is not worth it to
-        document the specific return types per event, but here are the rules:
+        This method should return a tuple with two items depicting the old
+        model that was stored in the cache and the new model that was created.
 
-        - Events that CREATE new data is not required to return any data.
-          If, for whatever reason, there is appropriate data it is urged to be
-          returned but will be discarded because of implementation details.
+        Because of the huge amounts of types of events that Discord sends it is
+        not worth it to document the specific return types per event, but here
+        are the rules to follow:
+
+        - Events that CREATE new data should return the model that was created.
+          Even if the data was not used to create a model, one should be
+          returned to then be dispatched to the user. Since there is no
+          previous data the first item will always be None.
 
         - Events that UPDATE existing data should return the older data if
-          present in the cache. That way it can be used by the user.
+          present in the cache. That way it can be used by the user. Both
+          items of the tuple may be used for the old model, and the new one.
 
         - Events that DELETE objects should return the existing data if it can
-          be found in the cache.
+          be found in the cache. This should be in the first item because it is
+          older data, the second item should be None because there is no new
+          model that is created.
 
-        - All other events, such as RESUMED or TYPING_START should return None
-          as it is simply discarded.
+        - All other events, such as RESUMED or TYPING_START which does not have
+          any data should return None for both items.
 
         Parameters:
             payload:
@@ -40,8 +50,118 @@ class Cache(Protocol):
                 Discord over the gateway.
 
         Returns:
-            The older cached data - that is now being updated by this event.
-            If there is no older data then None should be returned.
+            A tuple with two items (models) depicting the old model that was
+            popped and the new model that was created `(old, new)`.
         """
         ...
 
+    async def get_guild(self, guild: SupportsInt) -> Optional[Guild]:
+        """Get a guild model from cache.
+
+        Parameters:
+            guild: The ID of the guild to get data for.
+
+        Returns:
+            A guild model representation, if found in the cache.
+        """
+        ...
+
+    async def get_role(self, role: SupportsInt) -> Optional[Role]:
+        """Get a role from the cache.
+
+        Parameters:
+            role: The ID of the role to get the data for.
+
+        Returns:
+            The Role model representation, if found in the cache.
+        """
+        ...
+
+    async def get_channel(self, channel: SupportsInt) -> Optional[Channel]:
+        """Get a channel from the cache.
+
+        Parameters:
+            channel: The ID of the channel.
+
+        Returns:
+            The Channel model representation, if found in the cache.
+        """
+        ...
+
+    async def get_message(
+        self,
+        channel: Optional[SupportsInt],
+        message: SupportsInt
+    ) -> Optional[Message]:
+        """Get a message from the cache.
+
+        Parameters:
+            channel:
+                The ID of the channel the message is in, if it is available.
+                This can be None for the freedom of the user and since the
+                cache may not require it to look up the message.
+            message: The ID of the message.
+
+        Returns:
+            The Message model representation, if found in the cache.
+        """
+        ...
+
+    async def get_emoji(self, emoji: SupportsInt) -> Optional[Emoji]:
+        """Get an emoji from the cache.
+
+        Parameters:
+            emoji: The ID of the emoji.
+
+        Returns:
+            The Emoji model representation, if found in the cache.
+        """
+        ...
+
+    async def get_sticker(self, sticker: SupportsInt) -> Optional[Sticker]:
+        """Get a sticker from the cache.
+
+        Parameters:
+            sticker: The ID of the sticker.
+
+        Returns:
+            The Sticker model representation, if found in the cache.
+        """
+        ...
+
+    async def get_invite(self, invite: SupportsInt) -> Optional[Invite]:
+        """Get an invite from the cache.
+
+        Parameters:
+            invite: The ID of the invite.
+
+        Returns:
+            The Invite model representation, if found in the cache.
+        """
+        ...
+
+    async def get_member(self, guild: SupportsInt, user: SupportsInt) -> Optional[Member]:
+        """Get a member from a guild from the cache.
+
+        Parameters:
+            guild: The ID of the guild the user is in.
+            user: The ID of the user.
+
+        Returns:
+            The Member model representation, if found in the cache.
+        """
+        ...
+
+    async def get_user(self, user: SupportsInt) -> Optional[User]:
+        """Get a user from the cache.
+
+        Parameters:
+            user: The ID of the user.
+
+        Returns:
+            The User model representation, if found in the cache.
+        """
+        ...
+
+
+CacheProtocol = AsyncContextManager[Cache]
