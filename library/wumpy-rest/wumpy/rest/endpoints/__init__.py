abb1cde2480ba499debd00f7a22f379c261b2908
Bluenix <bluenixdev@gmail.com>
2022-02-25 00:28:46 +0100

    Import `InteractionRequester` in `__init__.py`

--- library/wumpy-rest/wumpy/rest/endpoints/__init__.py
+++ library/wumpy-rest/wumpy/rest/endpoints/__init__.py
@@ -3,13 +3,15 @@ from .channel import *
 from .commands import *
 from .gateway import *
 from .guild_template import *
+from .interactions import *
 from .sticker import *
 from .user import *
 from .webhook import *
 
 
 class APIClient(ApplicationCommandRequester, ChannelRequester, GatewayRequester,
-                GuildTemplateRequester, StickerRequester, UserRequester, WebhookRequester):
+                GuildTemplateRequester, InteractionRequester, StickerRequester, UserRequester,
+                WebhookRequester):
     """Requester class with all endpoints inherited."""
 
     __slots__ = ()
