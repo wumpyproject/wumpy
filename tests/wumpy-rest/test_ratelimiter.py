abb17eec79417eb2230755925681b094f0ad88d3
Bluenix <bluenixdev@gmail.com>
2022-02-24 23:09:48 +0100

    Make `wumpy-testing` an optional dependency

--- tests/wumpy-rest/test_ratelimiter.py
+++ tests/wumpy-rest/test_ratelimiter.py
@@ -1,5 +1,17 @@
+from typing import TYPE_CHECKING
+
+import pytest
 from wumpy.rest import DictRatelimiter
-from wumpy.testing.suites import RatelimiterSuite
+
+
+if TYPE_CHECKING:
+    from wumpy.testing.suites import RatelimiterSuite
+else:
+    __mod = pytest.importorskip(
+        'wumpy.testing.suites', reason='Wumpy-testing is required for the ratelimiter tests'
+    )
+    RatelimiterSuite = __mod.RatelimiterSuite
+    del __mod
 
 
 class TestDictRatelimiter(RatelimiterSuite):
