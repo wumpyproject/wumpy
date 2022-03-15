954d4b5158f52b2a2e09b4521f293af5dcddd92d
Bluenix <bluenixdev@gmail.com>
2022-03-15 12:48:56 +0100

    Strengthen the idea that middlewares are in a stack

--- library/wumpy-interactions/wumpy/interactions/commands/slash.py
+++ library/wumpy-interactions/wumpy/interactions/commands/slash.py
@@ -1,22 +1,15 @@
-from typing import (
-    Any, Awaitable, Callable, Dict, Generic, NamedTuple, TypeVar
-)
+from typing import Any, Awaitable, Callable, Generic, TypeVar
 
 from typing_extensions import ParamSpec
-from wumpy.models import (
-    CommandInteraction
-)
+from wumpy.models import CommandInteraction
 
 __all__ = ('Middleware', 'SlashCommand',)
 
 
 P = ParamSpec('P')
 RT = TypeVar('RT')
-
-
-class Middleware(NamedTuple):
-    cls: Callable[..., Callable[[CommandInteraction], Awaitable[object]]]
-    options: Dict[str, object]
+MiddlewareCallback = Callable[[CommandInteraction], Awaitable[object]]
+Middleware = Callable[[MiddlewareCallback], MiddlewareCallback]
 
 
 class SlashCommand(Generic[P, RT]):
@@ -28,8 +21,7 @@ class SlashCommand(Generic[P, RT]):
     def __init__(self, callback: Callable[P, Awaitable[RT]]) -> None:
         self.callback = callback
 
-        self.callstack = self._inner_call
-        self.middlewares = []
+        self._invoke_stack = self._inner_call
 
     async def __call__(self, *args: P.args, **kwds: P.kwargs) -> RT:
         return await self.callback(*args, **kwds)
@@ -49,26 +41,21 @@ class SlashCommand(Generic[P, RT]):
         Parameters:
             interaction: The interaction to invoke the command with.
         """
-        return await self.callstack(interaction)
+        return await self._invoke_stack(interaction)
 
-    def _build_callstack(self) -> None:
-        prev = self._inner_call
-        for cls, options in self.middlewares:
-            prev = cls(prev, **options)
-
-        self.callstack = prev
-
-    def add_middleware(self, middleware: Middleware) -> None:
-        """Add a middleware to the stack.
+    def push_middleware(self, middleware: Middleware) -> None:
+        """Push a middleware to the stack.
 
         Middlewares are a lowlevel functionality that allows heavily extensible
-        behaviour in how commands are invoked. Essentially middlewares are
-        before/after hooks for the command.
+        behaviour in how commands are invoked. They are essentially before
+        / after hooks for the command.
+
+        If you need to pass other options to the middleware, use `partial()`
+        from `functools`.
 
         Parameters:
             middleware:
                 An instance of the `Middleware` namedtuple with a callback and
                 dictionary of options to pass it when calling it.
         """
-        self.middlewares.append(middleware)
-        self._build_callstack()
+        self._invoke_stack = middleware(self._invoke_stack)
