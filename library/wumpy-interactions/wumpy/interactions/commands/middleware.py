13c30d0f116586e07a251fce9a3200016715335f
Bluenix <bluenixdev@gmail.com>
2022-03-17 17:36:29 +0100

    Add a max concurrency middleware helper decorator

--- library/wumpy-interactions/wumpy/interactions/commands/middleware.py
+++ library/wumpy-interactions/wumpy/interactions/commands/middleware.py
@@ -1,10 +1,17 @@
-from typing import Callable, TypeVar
-from functools import wraps, partial
+import time
+from enum import Enum
+from functools import partial, wraps
+from typing import Callable, Dict, Hashable, TypeVar
+from weakref import WeakValueDictionary
 
-from .base import CommandCallback, MiddlewareCallback
+import anyio
+
 from ..models import CommandInteraction
+from .base import CommandCallback, MiddlewareCallback
 
-__all__ = ('MiddlewareDecorator', 'CheckFailure', 'check')
+__all__ = (
+    'MiddlewareDecorator', 'CheckFailure', 'check', 'BucketType', 'max_concurrency',
+)
 
 
 CommandT = TypeVar('CommandT', bound=CommandCallback)
@@ -85,3 +92,105 @@ def check(predicate: MiddlewareCallback) -> MiddlewareDecorator:
         command.push_middleware(partial(CheckMiddleware, predicate=predicate))
         return command
     return wrapper
+
+
+class BucketType(Enum):
+    user = 0
+    member = 1
+
+    guild = 2
+    channel = 3
+
+    target = 7
+
+    def __call__(self, inter: CommandInteraction) -> Hashable:
+        cls = type(self)
+        return {
+            cls.user: inter.author.id,
+            cls.member: (inter.author.id, inter.guild_id),
+
+            cls.guild: inter.guild_id,
+            cls.channel: inter.channel_id,
+            cls.target: inter.target_id,
+        }[self]
+
+
+class MaxConcurrencyMiddleware:
+
+    call_next: MiddlewareCallback
+
+    number: int
+    key: Callable[[CommandInteraction], Hashable]
+
+    semaphores: 'WeakValueDictionary[Hashable, anyio.Semaphore]'
+
+    __slots__ = ('call_next', 'number', 'key', 'semaphores')
+
+    def __init__(
+        self,
+        call_next: MiddlewareCallback,
+        *,
+        number: int,
+        key: Callable[[CommandInteraction], Hashable],
+    ) -> None:
+        self.call_next = call_next
+
+        self.number = number
+        self.key = key
+
+        self.semaphores = WeakValueDictionary()
+
+    async def __call__(self, interaction: CommandInteraction) -> None:
+        key = self.key(interaction)
+
+        semaphore = self.semaphores.setdefault(key, anyio.Semaphore(self.number))
+
+        try:
+            semaphore.acquire_nowait()
+        except anyio.WouldBlock:
+            await interaction.defer()
+            await semaphore.acquire()
+
+        try:
+            await self.call_next(interaction)
+        finally:
+            semaphore.release()
+
+
+def max_concurrency(
+    number: int,
+    key: Callable[[CommandInteraction], Hashable]
+) -> MiddlewareDecorator:
+    """Create a middleware that limits concurrent uses of a command.
+
+    There are utility functions under `wumpy.interactions.BucketType` for the
+    `key` parameter.
+
+    Examples:
+
+        ```python
+        from wumpy import interactions
+        from wumpy.interactions import InteractionApp, CommandInteraction
+
+
+        app = InteractionApp(...)
+
+
+        @interactions.max_concurrency(1, interactions.BucketType.guild)
+        @app.command()
+        async def ping(interaction: CommandInteraction) -> None:
+            \"\"\"Pong!\"\"\"
+            await interaction.respond('Pong!')
+        ```
+
+    Parameters:
+        number: The number of concurrent uses of the command.
+        key: A callable to determine the key to use for the bucket.
+
+    Returns:
+        A decorator to apply to the application command.
+    """
+    def decorator(command: CommandT) -> CommandT:
+        command.push_middleware(partial(MaxConcurrencyMiddleware, key=key, number=number))
+        return command
+    return decorator
