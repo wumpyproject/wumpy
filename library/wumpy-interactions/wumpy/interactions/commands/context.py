bb1f9e9ee3d49099101a13c07426e8e395237417
Bluenix <bluenixdev@gmail.com>
2022-03-23 00:01:42 +0100

    Finish transition of context menus to command refactor

--- library/wumpy-interactions/wumpy/interactions/commands/context.py
+++ library/wumpy-interactions/wumpy/interactions/commands/context.py
@@ -1,14 +1,14 @@
 import inspect
-from functools import partial
-from typing import Any, Dict, Optional, TypeVar
+from typing import Any, List, Optional, TypeVar
 
-import anyio.abc
 from typing_extensions import ParamSpec
-from wumpy.models import CommandInteraction, InteractionMember, Message, User
+from wumpy.models import (
+    CommandInteractionOption, InteractionMember, Message, User
+)
 
 from ..errors import CommandSetupError
+from ..models import CommandInteraction
 from .base import Callback, CommandCallback
-from .option import CommandType
 
 __all__ = ('ContextMenuCommand', 'MessageCommand', 'UserCommand')
 
@@ -22,40 +22,60 @@ class ContextMenuCommand(CommandCallback[P, RT]):
 
     Attributes:
         name: Name of the context menu command.
-        argument:
-            The parameter annotation on the callback. This is used by
-            subclasses to get some more information about the type to use.
     """
 
-    argument: Any
-
     def __init__(self, callback: Callback[P, RT], *, name: Optional[str] = None) -> None:
-        super().__init__(callback, name=name)
+        self.name = name
 
-        self.argument = None
+        super().__init__(callback)
 
     def _verify_annotation(self, annotation: Any) -> None:
         # The point of this method is to raise TypeErrors if something is wrong
         # about the annotation.
         raise NotImplementedError
 
-    def _set_callback(self, function: Callback[P, RT]) -> None:
-        super()._set_callback(function)
+    def _process_callback(self, callback: Callback[P, RT]) -> None:
+        if self.name is None:
+            self.name = getattr(callback, '__name__', None)
 
-        if not inspect.iscoroutinefunction(function):
-            raise TypeError("'callback' has to be a coroutine function")
+        doc = inspect.getdoc(callback)
+        if self.description is None and doc is not None:
+            paragraps = doc.split('\n\n')
+            if paragraps:
+                # Similar to Markdown, we want to turn one newline character
+                # into spaces, and two characters into one.
+                self.description = paragraps[0].replace('\n', ' ')
 
-        signature = inspect.signature(function)
+        return super()._process_callback(callback)
 
-        if len(signature.parameters) != 2:
+    def _process_param(self, index: int, param: inspect.Parameter) -> None:
+        """Process a parameter of the set callback.
+
+        This method is called for each parameter of the callback when being
+        set, allowing for subclasses to hook into the process.
+
+        Parameters:
+            index: The index of the parameter.
+            param:
+                The parameter of the callback. Annotations have been resolved
+                and replaced with the actual type.
+        """
+        if index > 1:
             raise TypeError("'callback' has to have two parameters")
 
-        for param in signature.parameters.values():
-            if param.kind is param.kind.KEYWORD_ONLY:
-                raise TypeError("The parameters of 'callback' cannot be keyword-only")
+        if param.kind is param.kind.KEYWORD_ONLY:
+            raise TypeError("The parameters of 'callback' cannot be keyword-only")
 
-            if param.annotation is not param.empty:
-                self._verify_annotation(param.annotation)
+        if param.annotation is param.empty:
+            return  # The following code only inspects annotations
+
+        if index == 0:
+            if not isinstance(param.annotation, CommandInteraction):
+                raise TypeError(
+                    "The annotation of the first parameter must be a 'CommandInteraction'"
+                )
+        else:
+            self._verify_annotation(param.annotation)
 
     def resolve_value(self, interaction: CommandInteraction) -> Optional[Any]:
         """Resolve the single value for the interaction.
@@ -68,16 +88,16 @@ class ContextMenuCommand(CommandCallback[P, RT]):
         """
         raise NotImplementedError()
 
-    def handle_interaction(self, interaction: CommandInteraction, *, tg: anyio.abc.TaskGroup) -> None:
-        """Handle the interaction and call the registered callback.
-
-        If `resolve_value()` returns None the callback is not called.
-        """
+    async def _inner_call(
+        self,
+        interaction: CommandInteraction,
+        options: List[CommandInteractionOption]
+    ) -> None:
         value = self.resolve_value(interaction)
-        if value is None or self.callback is None:
+        if value is None:
             return
 
-        tg.start_soon(partial(self._call_wrapped, interaction, value))
+        await self.callback(interaction, value)  # type: ignore
 
 
 class MessageCommand(ContextMenuCommand[P, RT]):
@@ -85,9 +105,6 @@ class MessageCommand(ContextMenuCommand[P, RT]):
 
     Attributes:
         name: Name of the message command.
-        argument:
-            The parameter annotation on the callback. This isn't really used
-            by MessageCommand.
     """
 
     def _verify_annotation(self, annotation: Any) -> None:
@@ -109,14 +126,10 @@ class MessageCommand(ContextMenuCommand[P, RT]):
             raise CommandSetupError('Message command did not receive target ID.')
 
         message = interaction.resolved.messages.get(interaction.target_id)
-
-        assert message is not None, 'Discord did not send message data'
+        if message is None:
+            raise RuntimeError('Discord did not send message data for message command')
 
         return message
-
-    def to_dict(self) -> Dict[str, Any]:
-        """Turn the command into a dictionary to send to Discord."""
-        return {**super().to_dict(), 'type': CommandType.message.value}
 
 
 class UserCommand(ContextMenuCommand[P, RT]):
@@ -124,16 +137,20 @@ class UserCommand(ContextMenuCommand[P, RT]):
 
     Attributes:
         name: Name of the user command.
-        argument:
-            The parameter annotation on the callback. This is used to figure
-            out whether a member or user object should be passed.
     """
 
-    def _verify_annotation(self, annotation: Any) -> None:
-        if annotation in {CommandInteraction, User, InteractionMember}:
-            return
+    _annotation: Optional[Any]
 
-        raise TypeError(f"Invalid parameter annotation '{annotation}'")
+    def __init__(self, callback: Callback[P, RT], *, name: Optional[str] = None) -> None:
+        super().__init__(callback, name=name)
+
+        self._annotation = None
+
+    def _verify_annotation(self, annotation: Any) -> None:
+        if annotation not in {User, InteractionMember}:
+            raise TypeError(f"Invalid parameter annotation '{annotation}'")
+
+        self._annotation = annotation
 
     def resolve_value(self, interaction: CommandInteraction) -> Optional[Any]:
         """Resolve the user or member to pass to the callback.
@@ -142,24 +159,16 @@ class UserCommand(ContextMenuCommand[P, RT]):
             interaction: The interaction to resolve a user or member from.
 
         Returns:
-            A user or member object depending on what the `argument` attribute
-            is set to. If the `argument` attribute is set to a member but
-            Discord did not send member data this method will return None.
+            A user or member object depending on the annotation of the
+            callback the user command was created with.
         """
         if not interaction.target_id:
             raise CommandSetupError('User command did not receive target ID.')
 
-        # ContextMenuCommand saves the argument in preperation for real support
-        # of differentiating between InteractionUser and InteractionMember
-        if isinstance(self.argument, User):
+        if isinstance(self._annotation, User):
             target = interaction.resolved.users.get(interaction.target_id)
-        elif isinstance(self.argument, InteractionMember):
-            target = interaction.resolved.members.get(interaction.target_id)
         else:
-            raise CommandSetupError("User command's second argument incorrectly annotated")
+            # self._annotation is InteractionMember
+            target = interaction.resolved.members.get(interaction.target_id)
 
         return target
-
-    def to_dict(self) -> Dict[str, Any]:
-        """Turn the command into a dictionary to send to Discord."""
-        return {**super().to_dict(), 'type': CommandType.user.value}
