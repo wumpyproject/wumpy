wumpyproject/wumpy

commit e7dfe77d610dd9a99b7af606e50a1c6e8479c9c2

Hide internal commands dictionary in registrar

library/wumpy-interactions/wumpy/interactions/commands/registrar.py
@@ -13,7 +13,10 @@ __all__ = ['group', 'command', 'CommandRegistrar']
 
 P = ParamSpec('P')
 RT = TypeVar('RT', covariant=True)
-CommandUnion = Union[Command[P, RT], MessageCommand[P, RT], UserCommand[P, RT]]
+CommandUnion = Union[
+    Command[P, RT], MessageCommand[P, RT],
+    UserCommand[P, RT], SubcommandGroup
+]
 
 
 def group(
@@ -153,18 +156,14 @@ def command(
 
 
 class CommandRegistrar:
-    """Root registrar of command handlers.
+    """Root registrar of command handlers."""
 
-    Attributes:
-        commands: A dictionary of all registered commands.
-    """
-
-    commands: Dict[str, Command['...', object]]
+    _commands: Dict[str, CommandUnion['...', object]]
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
-        self.commands = {}
+        self._commands = {}
 
     async def invoke_command(self, interaction: CommandInteraction) -> None:
         """Handle the interaction and trigger appropriate callbacks.
@@ -176,7 +175,7 @@ class CommandRegistrar:
             interaction: The interaction to handle.
             tg: An anyio task group that will be used to launch callbacks.
         """
-        command = self.commands.get(interaction.name)
+        command = self._commands.get(interaction.name)
         if command is None:
             return
 
@@ -193,7 +192,18 @@ class CommandRegistrar:
         if command.name is None:
             raise ValueError('Command cannot be registered with no name')
 
-        self.commands[command.name] = command
+        self._commands[command.name] = command
+
+    def get_command(self, name: str) -> Optional[CommandUnion['...', object]]:
+        """Get a registered command from the registrar.
+
+        Parameters:
+            name: The name of the command to lookup.
+
+        Returns:
+            The command if found, otherwise `None`.
+        """
+        return self._commands.get(name)
 
     def remove_command(self, command: Command['...', object]) -> None:
         """Unregister a command from the internal dictionary.
@@ -210,13 +220,13 @@ class CommandRegistrar:
         if command.name is None:
             raise ValueError("Cannot unregister a command with no name")
 
-        if self.commands.get(command.name) != command:
+        if self._commands.get(command.name) != command:
             raise ValueError(
                 "'command' has not been registered previously or another"
                 "command is registered in its place"
             )
 
-        del self.commands[command.name]
+        del self._commands[command.name]
 
     def group(
         self,

commit ea1b8421105e86cf544ab21d959d994bface0433

Fix test failures introduced in e7dfe77d610dd9a99b7af606e50a1c6e8479c9c2
The change of quoting for ellipsis appears to be a bug in the typing module which wasn't accounted for.

library/wumpy-bot/wumpy/bot/extension.py
@@ -4,6 +4,7 @@ import sys
 from typing import Any, Callable, Dict, Optional, Union
 
 from wumpy.interactions import CommandRegistrar
+from wumpy.interactions.commands.slash import SubcommandGroup
 
 from .dispatch import EventDispatcher
 from .errors import ExtensionFailure
@@ -61,7 +62,7 @@ class Extension(CommandRegistrar, EventDispatcher):
                     target.add_listener(callback)
 
         if isinstance(target, CommandRegistrar):
-            for command in self.commands.values():
+            for command in self._commands.values():
                 target.add_command(command)
 
         self._data = data
@@ -75,7 +76,7 @@ class Extension(CommandRegistrar, EventDispatcher):
                     target.remove_listener(callback, event=annotation)
 
         if isinstance(target, CommandRegistrar):
-            for command in self.commands.values():
+            for command in self._commands.values():
                 target.remove_command(command)
 
 
@@ -108,8 +109,11 @@ class ExtensionLoader(CommandRegistrar, EventDispatcher):
                 if _is_submodule(callback.__module__, module):
                     self.remove_listener(callback, event=annotation)
 
-        for command in self.commands.values():
-            if _is_submodule(command.callback.__module__, module):
+        for command in self._commands.values():
+            if (
+                    not isinstance(command, SubcommandGroup)
+                    and _is_submodule(command.callback.__module__, module)
+            ):
                 self.remove_command(command)
 
     def load_extension(self, path: str, package: Optional[str] = None, **kwargs: Any) -> None:

library/wumpy-interactions/wumpy/interactions/commands/registrar.py
@@ -158,7 +158,7 @@ def command(
 class CommandRegistrar:
     """Root registrar of command handlers."""
 
-    _commands: Dict[str, CommandUnion['...', object]]
+    _commands: Dict[str, 'CommandUnion[..., object]']
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
@@ -194,7 +194,7 @@ class CommandRegistrar:
 
         self._commands[command.name] = command
 
-    def get_command(self, name: str) -> Optional[CommandUnion['...', object]]:
+    def get_command(self, name: str) -> Optional['CommandUnion[..., object]']:
         """Get a registered command from the registrar.
 
         Parameters:

tests/wumpy-interactions/commands/test_context.py
@@ -15,7 +15,7 @@ class TestContextCommand:
         async def test(interaction, argument):
             ...
 
-        assert registrar.commands['test'] == test
+        assert registrar.get_command('test') == test
 
     def test_non_async(self, commandtype: CommandType):
         registrar = CommandRegistrar()