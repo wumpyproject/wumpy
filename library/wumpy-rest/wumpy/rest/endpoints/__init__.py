a9328fbd91b93a81a049579ed7764b6fa1c18183
Bluenix <bluenixdev@gmail.com>
2022-05-01 21:02:26 +0200

    Expose and include forgotten GuildRequester

--- library/wumpy-rest/wumpy/rest/endpoints/__init__.py
+++ library/wumpy-rest/wumpy/rest/endpoints/__init__.py
@@ -2,6 +2,7 @@ from .base import *
 from .channel import *
 from .commands import *
 from .gateway import *
+from .guild import *
 from .guild_template import *
 from .interactions import *
 from .sticker import *
@@ -10,8 +11,8 @@ from .webhook import *
 
 
 class APIClient(ApplicationCommandRequester, ChannelRequester, GatewayRequester,
-                GuildTemplateRequester, InteractionRequester, StickerRequester, UserRequester,
-                WebhookRequester):
+                GuildRequester, GuildTemplateRequester, InteractionRequester, StickerRequester,
+                UserRequester, WebhookRequester):
     """Requester class with all endpoints inherited."""
 
     __slots__ = ()
