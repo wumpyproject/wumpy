780d67b6000c920999a9fafa7606a6d5290c5ac0
Bluenix <bluenixdev@gmail.com>
2022-05-01 14:45:58 +0200

    Remove STATELESS from wumpy-models
    The models will start getting the requester from context variables which will be added in a future commit.

--- library/wumpy-models/wumpy/models/utils.py
+++ library/wumpy-models/wumpy/models/utils.py
@@ -1,12 +1,11 @@
 from typing import Any, Mapping, Optional, Union
 
-from typing_extensions import Final, NoReturn, final
-from wumpy.rest.utils import MISSING
+from wumpy.rest.utils import MISSING as MISSING
 
 from .base import Snowflake
 
 # Reintroduce MISSING here so that it can be imported easier by models
-__all__ = ('MISSING', 'STATELESS', '_get_as_snowflake')
+__all__ = ('MISSING', '_get_as_snowflake')
 
 
 def _get_as_snowflake(data: Optional[Mapping[str, Any]], key: str) -> Optional[Snowflake]:
@@ -27,27 +26,3 @@ def _get_as_snowflake(data: Optional[Mapping[str, Any]], key: str) -> Optional[S
 
     value: Union[str, int, None] = data.get(key)
     return Snowflake(int(value)) if value is not None else None
-
-
-class SatelessException(RuntimeError):
-    """Exception signaling that the model is sateless.
-
-    This signals that no REST-API related methods can be used, as it is raised
-    if that is attempted. Allowing models to be fully stateless without loosing
-    static typing or complicating code with if-statements.
-
-    The purpose of this is to allow models to be user-constructed with nothing
-    but the payload from Discord (not requiring the APIClient).
-    """
-    def __init__(self) -> None:
-        super().__init__('Cannot call make an API call on a stateless model')
-
-
-@final
-class StatelessType:
-    """Class to raise exceptions when attributes are accessed."""
-    def __getattribute__(self, _: str) -> NoReturn:
-        raise SatelessException()
-
-
-STATELESS: Final[Any] = StatelessType()
