b9ff47ad58e5365b83e20e2f3b72616776be41e2
Bluenix <bluenixdev@gmail.com>
2022-05-03 22:24:13 +0200

    Rework existing channel events into new design

--- library/wumpy-bot/wumpy/bot/events/channel.py
+++ library/wumpy-bot/wumpy/bot/events/channel.py
@@ -1,14 +1,19 @@
+from dataclasses import dataclass
 from datetime import datetime, timezone
-from typing import Any, ClassVar, Dict, Optional
+from typing import Any, ClassVar, Dict, Optional, Tuple
 
+from typing_extensions import Self
 from wumpy.models import Snowflake
+from wumpy.models.member import Member
 
+from ..bot import get_bot
 from ..dispatch import Event
 from ..utils import _get_as_snowflake
 
 __all__ = ('TypingEvent', 'ChannelPinsUpdateEvent')
 
 
+@dataclass(frozen=True)
 class TypingEvent(Event):
     """Dispatched when a user starts typing in a channel."""
 
@@ -17,21 +22,42 @@ class TypingEvent(Event):
     guild_id: Optional[Snowflake]
 
     timestamp: datetime
-    member: Optional[Dict[str, Any]]
+    member: Optional[Member]
 
     NAME: ClassVar[str] = "TYPING_START"
 
     __slots__ = ('user_id', 'channel_id', 'guild_id', 'timestamp', 'member')
 
-    def __init__(self, data: Dict[str, Any]) -> None:
-        self.user_id = Snowflake(data['d']['user_id'])
-        self.channel_id = Snowflake(data['d']['channel_id'])
-        self.guild_id = _get_as_snowflake(data['d'], 'guild_id')
+    @classmethod
+    async def from_payload(
+            cls,
+            payload: Dict[str, Any],
+            cached: Tuple[Optional[Any], Optional[Any]] = (None, None)
+    ) -> Self:
+        data = payload['d']
 
-        self.timestamp = datetime.fromtimestamp(data['d']['timestamp'], tz=timezone.utc)
-        self.member = data['d'].get('member')
+        member = None
+        if data.get('member') is not None:
+            member = await get_bot().cache.get_member(data['guild_id'], data['user_id'])
+
+        if member is None:
+            user = await get_bot().cache.get_user(data['user_id'])
+            if user is not None:
+                member = Member.from_user(user, data['member'])
+            else:
+                member = Member.from_data(data['member'].get('user'), data['member'])
+
+        return cls(
+            user_id=Snowflake(data['user_id']),
+            channel_id=Snowflake(data['channel_id']),
+            guild_id=_get_as_snowflake(data, 'guild_id'),
+
+            timestamp=datetime.fromtimestamp(data['timestamp'], tz=timezone.utc),
+            member=member
+        )
 
 
+@dataclass(frozen=True)
 class ChannelPinsUpdateEvent(Event):
     """Dispatched when a channel's pins are updated.
 
@@ -43,17 +69,28 @@ class ChannelPinsUpdateEvent(Event):
     """
 
     channel_id: Snowflake
+    guild_id: Optional[Snowflake]
+
     last_pin_timestamp: Optional[datetime]
 
     NAME: ClassVar[str] = "CHANNEL_PINS_UPDATE"
 
-    __slots__ = ('channel_id', 'last_pin_timestamp')
+    __slots__ = ('channel_id', 'guild_id', 'last_pin_timestamp')
 
-    def __init__(self, data: Dict[str, Any]) -> None:
-        self.channel_id = Snowflake(data['d']['channel_id'])
+    @classmethod
+    async def from_payload(
+            cls,
+            payload: Dict[str, Any],
+            cached: Tuple[Optional[Any], Optional[Any]] = (None, None)
+    ) -> Self:
+        data = payload['d']
 
-        timestamp = data['d'].get('last_pin_timestamp')
-        if timestamp:
-            self.last_pin_timestamp = datetime.fromtimestamp(timestamp, tz=timezone.utc)
-        else:
-            self.last_pin_timestamp = None
+        last_pin = None
+        if data.get('last_pin_timestamp') is not None:
+            last_pin = datetime.fromisoformat(data['last_pin_timestamp'])
+
+        return cls(
+            channel_id=Snowflake(int(payload['d']['channel_id'])),
+            guild_id=_get_as_snowflake(data, 'guild_id'),
+            last_pin_timestamp=last_pin
+        )
