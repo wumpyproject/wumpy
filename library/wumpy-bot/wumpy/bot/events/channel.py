0506dfc274e8286554c2457d3f0133edb8e5c546
Bluenix <bluenixdev@gmail.com>
2022-05-15 02:47:27 +0200

    Use typings for gateway events

--- library/wumpy-bot/wumpy/bot/events/channel.py
+++ library/wumpy-bot/wumpy/bot/events/channel.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime, timezone
-from typing import Any, ClassVar, Dict, Optional, Tuple
+from typing import Any, ClassVar, Dict, Literal, Optional, Tuple
 
 from typing_extensions import Self
-from wumpy.models import Snowflake
-from wumpy.models.member import Member
+from discord_typings import TypingStartData
+from discord_typings.gateway import ChannelPinsUpdateData
+from wumpy.models import Snowflake, User, Member
 
 from ..bot import get_bot
 from ..dispatch import Event
@@ -22,6 +23,8 @@ class TypingEvent(Event):
     guild_id: Optional[Snowflake]
 
     timestamp: datetime
+
+    user: Optional[User]
     member: Optional[Member]
 
     NAME: ClassVar[str] = "TYPING_START"
@@ -31,36 +34,44 @@ class TypingEvent(Event):
     @classmethod
     async def from_payload(
             cls,
-            payload: Dict[str, Any],
+            payload: TypingStartData,
             cached: Tuple[Optional[Any], Optional[Any]] = (None, None)
     ) -> Self:
-        data = payload['d']
         cache = get_bot().cache
 
-        user_id = Snowflake(data['user_id'])
-        guild_id = _get_as_snowflake(data, 'guild_id')
+        user_id = Snowflake(payload['user_id'])
+        guild_id = _get_as_snowflake(payload, 'guild_id')
+
+        user = None
+        if not cache.remote:
+            user = await cache.get_user(payload['user_id'])
 
         member = None
         if guild_id is not None and not cache.remote:
-            member = await get_bot().cache.get_member(guild_id.id, user_id.id)
+            member = await cache.get_member(guild_id.id, user_id.id)
 
-        if member is None:
-            user = None
-            if not cache.remote:
-                user = await get_bot().cache.get_user(data['user_id'])
-
+        member_data = payload.get('member')
+        if member is None and member_data is not None:
             if user is not None:
-                member = Member.from_user(user, data['member'])
+                member = Member.from_user(user, member_data)
             else:
-                member = Member.from_data(data['member'])
+                member = Member.from_data(member_data)
+
+            # The user data may have been inside of the member object. If it
+            # wasn't - and user is currently None - we would've gotten an error
+            # and this code won't run.
+            if user is None:
+                user = member.user
 
         return cls(
             user_id=user_id,
-            channel_id=Snowflake(data['channel_id']),
+            channel_id=Snowflake(payload['channel_id']),
             guild_id=guild_id,
 
-            timestamp=datetime.fromtimestamp(data['timestamp'], tz=timezone.utc),
-            member=member
+            timestamp=datetime.fromtimestamp(payload['timestamp'], tz=timezone.utc),
+
+            user=user,
+            member=member,
         )
 
 
@@ -87,17 +98,16 @@ class ChannelPinsUpdateEvent(Event):
     @classmethod
     async def from_payload(
             cls,
-            payload: Dict[str, Any],
+            payload: ChannelPinsUpdateData,
             cached: Tuple[Optional[Any], Optional[Any]] = (None, None)
     ) -> Self:
-        data = payload['d']
 
-        last_pin = None
-        if data.get('last_pin_timestamp') is not None:
-            last_pin = datetime.fromisoformat(data['last_pin_timestamp'])
+        last_pin = payload.get('last_pin_timestamp')
+        if last_pin is not None:
+            last_pin = datetime.fromisoformat(last_pin)
 
         return cls(
-            channel_id=Snowflake(int(payload['d']['channel_id'])),
-            guild_id=_get_as_snowflake(data, 'guild_id'),
+            channel_id=Snowflake(int(payload['channel_id'])),
+            guild_id=_get_as_snowflake(payload, 'guild_id'),
             last_pin_timestamp=last_pin
         )
