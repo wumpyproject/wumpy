2d16622e155f8564245ec7db4f47db0afdff0c7e
Bluenix <bluenixdev@gmail.com>
2022-07-07 19:05:51 +0200

    Remove duplicate APIClient from `endpoints/`

--- library/wumpy-rest/wumpy/rest/endpoints/__init__.py
+++ library/wumpy-rest/wumpy/rest/endpoints/__init__.py
@@ -8,11 +8,3 @@ from .interactions import *
 from .sticker import *
 from .user import *
 from .webhook import *
-
-
-class APIClient(ApplicationCommandRequester, ChannelRequester, GatewayRequester,
-                GuildRequester, GuildTemplateRequester, InteractionRequester, StickerRequester,
-                UserRequester, WebhookRequester, HTTPXRequester):
-    """Requester class with all endpoints inherited."""
-
-    __slots__ = ()
