220e4908ccee27d0215dfde70339210c15ac6919
Bluenix <bluenixdev@gmail.com>
2022-07-08 04:17:12 +0200

    Move option helpers into `option.py` file

--- library/wumpy-interactions/wumpy/interactions/commands/option.py
+++ library/wumpy-interactions/wumpy/interactions/commands/option.py
@@ -1,7 +1,8 @@
 import inspect
 from enum import Enum
 from typing import (
-    Any, AnyStr, ClassVar, Dict, List, Optional, Tuple, Type, Union
+    Any, AnyStr, Callable, ClassVar, Dict, List, Optional, Tuple, Type,
+    TypeVar, Union
 )
 
 from typing_extensions import Annotated, Literal, get_args, get_origin
@@ -10,9 +11,13 @@ from wumpy.models import (
     InteractionChannel, InteractionMember, User
 )
 
-from ..errors import CommandSetupError
+from .._errors import CommandSetupError
+from . import _slash
 
-__all__ = ('CommandType', 'OptionClass')
+__all__ = ('CommandType', 'Option', 'option', 'OptionClass')
+
+
+T = TypeVar('T', bound='_slash.Command')
 
 
 class CommandType(Enum):
@@ -88,6 +93,105 @@ class _MISSING_DEFAULT_TYPE:
 
 _MISSING_DEFAULT = _MISSING_DEFAULT_TYPE()
 del _MISSING_DEFAULT_TYPE
+
+
+# The reason for this function is because we need the "return type" to be Any.
+# If we'd use the class directly then the following would break type checking
+# def command(number: int = Option(description='A number'))
+# 'int' and 'Option' is incompatible. This function avoids that
+def Option(
+    default: Any = _MISSING_DEFAULT,
+    *,
+    name: Optional[str] = None,
+    description: Optional[str] = None,
+    required: Optional[bool] = None,
+    choices: Union[List[Union[str, int, float]], Dict[str, Union[str, int, float]], None] = None,
+    min: Optional[int] = None,
+    max: Optional[int] = None,
+    type: Optional[Any] = None,
+    cls: Optional[Type[Any]] = None
+) -> Any:
+    """Interaction option, should be set as a default to a parameter.
+
+    The `cls` parameter can be used if you want to use a custom Option
+    class, you can use `functools.partial()` as to not repeat the kwarg.
+
+    Parameters:
+        default:
+            Default value when the option is not passed, makes the option
+            optional so that it can be omitted.
+        name:
+            Name of the option in the Discord client. By default it uses
+            the name of the parameter.
+        description: Description of the option.
+        required:
+            Whether the option can be omitted. If a default is passed this is
+            automatically set implicitly.
+        choices: Set choices that the user can pick from in the Discord client.
+        min: Smallest number that can be entered for number types.
+        max: Biggest number that can be entered for number types.
+        type:
+            The type of the option, overriding the annotation. This can be
+            a `ApplicationCommandOption` value or any type.
+        cls: The class to use, defaults to `OptionClass`.
+
+    Returns:
+        The `cls` parameter (`OptionClass` by default) disguised as
+        `typing.Any`. This way this function can be used as a default without
+        violating static type checkers.
+    """
+    return (cls or Option)(
+        default, name=name, description=description,
+        required=required, choices=choices,
+        min=min, max=max, type=type
+    )
+
+
+# This is different from the above function as this is a decorator meant
+# to be used on command instances.
+def option(
+    param: str,
+    name: Optional[str] = None,
+    description: Optional[str] = None,
+    required: Optional[bool] = None,
+    choices: Union[List[Union[str, int, float]], Dict[str, Union[str, int, float]], None] = None,
+    min: Optional[int] = None,
+    max: Optional[int] = None,
+    type: Optional[Any] = None
+) -> Callable[[T], T]:
+    """Option decorator for updating an option.
+
+    This decorator needs to be placed outside of the command decorator.
+
+    Parameters:
+        param: The name of the parameter for this option.
+        name: The new name of the option.
+        description: The new description of the option.
+        required: Whether the option should be able to be omitted.
+        choices: Set choices that the user needs to pick from.
+        min: Smallest number that can be entered for number types.
+        max: Biggest number that can be entered for number types.
+        type: New type of the option, overriding the annotation.
+
+    Exceptions:
+        ValueError: This decorator was not used on a command.
+    """
+    # Because of the fact that we delete the type variable below, it won't be
+    # available when this function is created.
+    def decorator(command: 'T') -> 'T':
+        if not isinstance(command, _slash.Command):
+            raise TypeError(
+                "The 'option' decorator can only be used on commands."
+            )
+
+        command.update_option(
+            param, name=name, description=description,
+            required=required, choices=choices, min=min, max=max, type=type
+        )
+
+        # Return the command again for chaining.
+        return command
+    return decorator
 
 
 class OptionClass:
